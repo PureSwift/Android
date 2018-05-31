651447fef2fd6ab95c03cfad2648a9cb94809381
jmarkstar <mestrella.1991@gmail.com>
2018-05-31 14:52:32 -0500

    Finished `BluetoothGattService`

--- Sources/BluetoothGattService.swift
+++ Sources/BluetoothGattService.swift
@@ -8,10 +8,16 @@
 import Foundation
 import Bluetooth
 import java_swift
+import java_util
 
 public extension Android.Bluetooth {
     
     public typealias GattService = AndroidBluetoothGattService
+}
+
+public extension Android.Bluetooth.GattService {
+    
+    public typealias ServiceType = AndroidBluetoothLowEnergyServiceType
 }
 
 /**
@@ -20,6 +26,10 @@ public extension Android.Bluetooth {
  * Gatt Service contains a collection of BluetoothGattCharacteristic, as well as referenced services.
  */
 public final class AndroidBluetoothGattService: JavaObject {
+    
+    public required init( javaObject: jobject? ) {
+        super.init(javaObject: javaObject)
+    }
     
     public convenience init?( casting object: java_swift.JavaObject,
                               _ file: StaticString = #file,
@@ -32,8 +42,55 @@ public final class AndroidBluetoothGattService: JavaObject {
         }
     }
     
-    public required init( javaObject: jobject? ) {
-        super.init(javaObject: javaObject)
+    /**
+     * Create a new BluetoothGattService.
+     *
+     * Requires BLUETOOTH permission.
+     */
+    public convenience init(uuid: java_util.UUID, serviceType: ServiceType){
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            JNIType.toJava(value: uuid, locals: &__locals),
+            jvalue(i: jint(serviceType.rawValue))
+        ]
+        
+        let __object = JNIMethod.NewObject(
+            className: JNICache.className,
+            classCache: &JNICache.jniClass,
+            methodSig: "(Ljava/util/UUID;I)V",
+            methodCache: &JNICache.MethodID.init_method,
+            args: &__args,
+            locals: &__locals )
+        
+        self.init( javaObject: __object )
+        
+        JNI.DeleteLocalRef( __object )
+    }
+    
+    internal var mCharacteristics: java_util.List? {
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "mCharacteristics",
+                                                  fieldType: "L",
+                                                  fieldCache: &JNICache.FieldID.mCharacteristics,
+                                                  object: javaObject)
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return java_util.ListForward(javaObject: __value)
+        }
+    }
+    
+    internal var mIncludedServices: java_util.List? {
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "mIncludedServices",
+                                                  fieldType: "L",
+                                                  fieldCache: &JNICache.FieldID.mIncludedServices,
+                                                  object: javaObject)
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return java_util.ListForward(javaObject: __value)
+        }
     }
 }
 
@@ -41,12 +98,200 @@ public final class AndroidBluetoothGattService: JavaObject {
 
 public extension AndroidBluetoothGattService {
     
+    /// RFCOMM socket
+    internal static var SERVICE_TYPE_PRIMARY: Int {
+        
+        get {
+            
+            let __value = JNIField.GetStaticIntField(
+                fieldName: "SERVICE_TYPE_PRIMARY",
+                fieldType: "I",
+                fieldCache: &JNICache.FieldID.SERVICE_TYPE_PRIMARY,
+                className: JNICache.className,
+                classCache: &JNICache.jniClass )
+            
+            return Int(__value)
+        }
+    }
+    
+    /// RFCOMM socket
+    internal static var SERVICE_TYPE_SECONDARY: Int {
+        
+        get {
+            
+            let __value = JNIField.GetStaticIntField(
+                fieldName: "SERVICE_TYPE_SECONDARY",
+                fieldType: "I",
+                fieldCache: &JNICache.FieldID.SERVICE_TYPE_SECONDARY,
+                className: JNICache.className,
+                classCache: &JNICache.jniClass )
+            
+            return Int(__value)
+        }
+    }
 }
 
 // MARK: Methods
 
 public extension AndroidBluetoothGattService {
     
+    public func addCharacteristic(characteristic: Android.Bluetooth.GattCharacteristic) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args:[jvalue] = [
+            JNIType.toJava(value: characteristic, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "addCharacteristic",
+                                                   methodSig: "(Landroid/bluetooth/BluetoothGattCharacteristic;)Z",
+                                                   methodCache: &JNICache.MethodID.addCharacteristic,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    public func addService(service: Android.Bluetooth.GattService) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args:[jvalue] = [
+            JNIType.toJava(value: service, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "addCharacteristic",
+                                                   methodSig: "(Landroid/bluetooth/BluetoothGattService;)Z",
+                                                   methodCache: &JNICache.MethodID.addService,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    public func getCharacteristic(uuid: java_util.UUID) -> Android.Bluetooth.GattCharacteristic {
+        
+        var __locals = [jobject]()
+        
+        var __args:[jvalue] = [
+            JNIType.toJava(value: uuid, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                   methodName: "getCharacteristic",
+                                                   methodSig: "(Ljava/util/UUID;)Landroid/bluetooth/BluetoothGattCharacteristic;",
+                                                   methodCache: &JNICache.MethodID.getCharacteristic,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        defer { JNI.DeleteLocalRef(__return) }
+        
+        return Android.Bluetooth.GattCharacteristic(javaObject: __return)
+    }
+    
+    public func getCharacteristics() -> java_util.List? {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                  methodName: "getCharacteristic",
+                                                  methodSig: "()Ljava/util/List;",
+                                                  methodCache: &JNICache.MethodID.getCharacteristics,
+                                                  args: &__args,
+                                                  locals: &__locals)
+        defer { JNI.DeleteLocalRef(__return) }
+        
+        return java_util.ListForward(javaObject: __return)
+    }
+    
+    public func getIncludedServices() -> java_util.List? {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                  methodName: "getIncludedServices",
+                                                  methodSig: "()Ljava/util/List;",
+                                                  methodCache: &JNICache.MethodID.getIncludedServices,
+                                                  args: &__args,
+                                                  locals: &__locals)
+        defer { JNI.DeleteLocalRef(__return) }
+        
+        return java_util.ListForward(javaObject: __return)
+    }
+    
+    public func getInstanceId() -> Int {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallIntMethod(object: javaObject,
+                                                   methodName: "getInstanceId",
+                                                   methodSig: "()I",
+                                                   methodCache: &JNICache.MethodID.getInstanceId,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return Int(__return)
+    }
+    
+    public func getType() -> Android.Bluetooth.GattService.ServiceType {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallIntMethod(object: javaObject,
+                                               methodName: "getType",
+                                               methodSig: "()I",
+                                               methodCache: &JNICache.MethodID.getType,
+                                               args: &__args,
+                                               locals: &__locals)
+        
+        return Android.Bluetooth.GattService.ServiceType(rawValue: Int(__return))
+    }
+    
+    public func getUuid() -> java_util.UUID {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                               methodName: "getUuid",
+                                               methodSig: "()I",
+                                               methodCache: &JNICache.MethodID.getUuid,
+                                               args: &__args,
+                                               locals: &__locals)
+        return java_util.UUID(javaObject: __return)
+    }
+}
+
+// MARK: - Supportinh types
+
+public extension AndroidBluetoothGattService {
+    
+    /// LE Secondary Phy
+    public struct AndroidBluetoothLowEnergyServiceType: RawRepresentable {
+        
+        public let rawValue: Int
+        
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+        
+        /**
+         * Primary service.
+         */
+        public static let primary = AndroidBluetoothGattService.ServiceType(rawValue: AndroidBluetoothGattService.SERVICE_TYPE_PRIMARY)
+        
+        /**
+         * Secondary service (included by primary services).
+         */
+        public static let secondary = AndroidBluetoothGattService.ServiceType(rawValue: AndroidBluetoothGattService.SERVICE_TYPE_SECONDARY)
+    }
 }
 
 // MARK: - JNICache
@@ -69,6 +314,7 @@ internal extension AndroidBluetoothGattService {
             
             static var SERVICE_TYPE_PRIMARY: jfieldID?
             static var SERVICE_TYPE_SECONDARY: jfieldID?
+            
             static var mCharacteristics: jfieldID?
             static var mIncludedServices: jfieldID?
         }
