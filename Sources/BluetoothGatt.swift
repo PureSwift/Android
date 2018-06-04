139c654c32be071e33bec260209f0994024e168e
jmarkstar <mestrella.1991@gmail.com>
2018-06-04 11:58:23 -0500

    Finished `BluetoothGatt `

--- Sources/BluetoothGatt.swift
+++ Sources/BluetoothGatt.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Bluetooth
 import java_swift
+import java_util
 
 public extension Android.Bluetooth {
     
@@ -241,6 +242,382 @@ internal extension AndroidBluetoothGatt {
 
 public extension AndroidBluetoothGatt {
     
+    /**
+     * Cancels a reliable write transaction for a given device.
+     */
+    public func abortReliableWrite() {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        JNIMethod.CallVoidMethod(object: javaObject,
+                                   methodName: "abortReliableWrite",
+                                   methodSig: "()V",
+                                   methodCache: &JNICache.MethodID.abortReliableWrite,
+                                   args: &__args,
+                                   locals: &__locals)
+    }
+    
+    /**
+     * Initiates a reliable write transaction for a given remote device.
+     */
+    public func beginReliableWrite() -> Bool {
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                 methodName: "beginReliableWrite",
+                                 methodSig: "()Z",
+                                 methodCache: &JNICache.MethodID.beginReliableWrite,
+                                 args: &__args,
+                                 locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Close this Bluetooth GATT client.
+     */
+    public func close() {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        JNIMethod.CallVoidMethod(object: javaObject,
+                                 methodName: "close",
+                                 methodSig: "()V",
+                                 methodCache: &JNICache.MethodID.close,
+                                 args: &__args,
+                                 locals: &__locals)
+    }
+    
+    /**
+     * Close this Bluetooth GATT client.
+     */
+    public func connect() -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                 methodName: "connect",
+                                 methodSig: "()Z",
+                                 methodCache: &JNICache.MethodID.connect,
+                                 args: &__args,
+                                 locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Disconnects an established connection, or cancels a connection attempt currently in progress.
+     */
+    public func disconnect() {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        JNIMethod.CallVoidMethod(object: javaObject,
+                                 methodName: "disconnect",
+                                 methodSig: "()V",
+                                 methodCache: &JNICache.MethodID.disconnect,
+                                 args: &__args,
+                                 locals: &__locals)
+    }
+    
+    /**
+     * Discovers services offered by a remote device as well as their characteristics and descriptors.
+     */
+    public func discoverServices() -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "discoverServices",
+                                                   methodSig: "()Z",
+                                                   methodCache: &JNICache.MethodID.discoverServices,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Executes a reliable write transaction for a given remote device.
+     */
+    public func executeReliableWrite() -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "executeReliableWrite",
+                                                   methodSig: "()Z",
+                                                   methodCache: &JNICache.MethodID.executeReliableWrite,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Return the remote bluetooth device this GATT client targets to.
+     */
+    public func getDevice() -> Android.Bluetooth.Device {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                   methodName: "getDevice",
+                                                   methodSig: "()Landroid/bluetooth/BluetoothDevice;",
+                                                   methodCache: &JNICache.MethodID.getDevice,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        
+        return Android.Bluetooth.Device(javaObject: __return)
+    }
+    
+    /**
+     * Returns a BluetoothGattService, if the requested UUID is supported by the remote device.
+     */
+    public func getService(uuid: java_util.UUID) -> Android.Bluetooth.GattService {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            JNIType.toJava(value: uuid, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                  methodName: "getService",
+                                                  methodSig: "()Landroid/bluetooth/BluetoothGattService;",
+                                                  methodCache: &JNICache.MethodID.getService,
+                                                  args: &__args,
+                                                  locals: &__locals)
+        
+        return Android.Bluetooth.GattService(javaObject: __return)
+    }
+    
+    /**
+     * Returns a list of GATT services offered by the remote device.
+     */
+    public func getServices() -> List? {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallObjectMethod(object: javaObject,
+                                                  methodName: "getServices",
+                                                  methodSig: "()Ljava/util/List;",
+                                                  methodCache: &JNICache.MethodID.getServices,
+                                                  args: &__args,
+                                                  locals: &__locals)
+        return ListForward(javaObject: __return)
+    }
+    
+    /**
+     * Reads the requested characteristic from the associated remote device.
+     */
+    public func readCharacteristic(characteristic: Android.Bluetooth.GattCharacteristic) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            JNIType.toJava(value: characteristic, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "readCharacteristic",
+                                                   methodSig: "(Landroid/bluetooth/BluetoothGattCharacteristic;)Z",
+                                                   methodCache: &JNICache.MethodID.readCharacteristic,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Reads the value for a given descriptor from the associated remote device.
+     */
+    public func readDescriptor(descriptor: Android.Bluetooth.GattDescriptor) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            JNIType.toJava(value: descriptor, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "readDescriptor",
+                                                   methodSig: "(Landroid/bluetooth/BluetoothGattDescriptor;)Z",
+                                                   methodCache: &JNICache.MethodID.readDescriptor,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Read the current transmitter PHY and receiver PHY of the connection.
+     */
+    public func readPhy() {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        JNIMethod.CallVoidMethod(object: javaObject,
+                                 methodName: "readPhy",
+                                 methodSig: "()V",
+                                 methodCache: &JNICache.MethodID.readPhy,
+                                 args: &__args,
+                                 locals: &__locals)
+    }
+    
+    /**
+     * Read the RSSI for a connected remote device.
+     */
+    public func readRemoteRssi() -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args = [jvalue](repeating: jvalue(), count: 1)
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "readRemoteRssi",
+                                                   methodSig: "()Z",
+                                                   methodCache: &JNICache.MethodID.readRemoteRssi,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Request a connection parameter update.
+     */
+    public func requestConnectionPriority(connectionPriority: Int) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            jvalue(i: jint(connectionPriority))
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "requestConnectionPriority",
+                                                   methodSig: "(I)Z",
+                                                   methodCache: &JNICache.MethodID.requestConnectionPriority,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Request an MTU size used for a given connection.
+     */
+    public func requestMtu(mtu: Int) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            jvalue(i: jint(mtu))
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "requestMtu",
+                                                   methodSig: "(I)Z",
+                                                   methodCache: &JNICache.MethodID.requestMtu,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Enable or disable notifications/indications for a given characteristic.
+     */
+    public func setCharacteristicNotification(characteristic: Android.Bluetooth.GattCharacteristic, enable: Bool) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            JNIType.toJava(value: characteristic, locals: &__locals),
+            jvalue(z: jboolean(enable ?  JNI_TRUE : JNI_FALSE))
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "setCharacteristicNotification",
+                                                   methodSig: "(Landroid/bluetooth/BluetoothGattCharacteristic;Z)Z",
+                                                   methodCache: &JNICache.MethodID.setCharacteristicNotification,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Set the preferred connection PHY for this app.
+     */
+    public func setPreferredPhy(txPhy: Int, rxPhy: Int, phyOptions: Int)  {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            jvalue(i: jint(txPhy)),
+            jvalue(i: jint(rxPhy)),
+            jvalue(i: jint(phyOptions))
+        ]
+        
+        JNIMethod.CallVoidMethod(object: javaObject,
+                                   methodName: "setPreferredPhy",
+                                   methodSig: "(III)V",
+                                   methodCache: &JNICache.MethodID.setPreferredPhy,
+                                   args: &__args,
+                                   locals: &__locals)
+    }
+    
+    /**
+     * Writes a given characteristic and its values to the associated remote device.
+     */
+    public func writeCharacteristic(characteristic: Android.Bluetooth.GattCharacteristic) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            JNIType.toJava(value: characteristic, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "writeCharacteristic",
+                                                   methodSig: "(Landroid/bluetooth/BluetoothGattCharacteristic;)Z",
+                                                   methodCache: &JNICache.MethodID.writeCharacteristic,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
+    
+    /**
+     * Write the value of a given descriptor to the associated remote device.
+     */
+    public func writeDescriptor(descriptor: Android.Bluetooth.GattDescriptor) -> Bool {
+        
+        var __locals = [jobject]()
+        
+        var __args: [jvalue] = [
+            JNIType.toJava(value: descriptor, locals: &__locals)
+        ]
+        
+        let __return = JNIMethod.CallBooleanMethod(object: javaObject,
+                                                   methodName: "writeDescriptor",
+                                                   methodSig: "(Landroid/bluetooth/BluetoothGattDescriptor;)Z",
+                                                   methodCache: &JNICache.MethodID.writeDescriptor,
+                                                   args: &__args,
+                                                   locals: &__locals)
+        return __return != jboolean(JNI_FALSE)
+    }
 }
 
 // MARK: - JNICache
@@ -292,7 +669,7 @@ internal extension AndroidBluetoothGatt {
             static var getDevice: jmethodID?
             static var getDevicesMatchingConnectionStates: jmethodID?
             static var getService: jmethodID?
-            static var getServices2: jmethodID?
+            static var getServices: jmethodID?
             static var readCharacteristic: jmethodID?
             static var readDescriptor: jmethodID?
             static var readPhy: jmethodID?
