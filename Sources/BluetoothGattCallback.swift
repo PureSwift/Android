0d8ec7137e86aee691a37824895ffb1b7f8842a2
jmarkstar <mestrella.1991@gmail.com>
2018-06-15 16:21:33 -0500

    Working on `BluetoothGattCallback`

--- Sources/BluetoothGattCallback.swift
+++ Sources/BluetoothGattCallback.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import java_swift
+import java_util
+import JNI
+
+public extension Android.Bluetooth.LE {
+    
+    public typealias GattCallback = AndroidBluetoothGattCallback
+}
+
+/// his abstract class is used to implement BluetoothGatt callbacks.
+public protocol AndroidBluetoothGattCallback: JavaProtocol {
+    
+    func onCharacteristicChanged(gatt: Android.Bluetooth.Gatt, characteristic: Android.Bluetooth.GattCharacteristic)
+    
+    func onCharacteristicRead(gatt: Android.Bluetooth.Gatt, characteristic: Android.Bluetooth.GattCharacteristic, status: Android.Bluetooth.Gatt.Status)
+    
+    func onCharacteristicWrite(gatt: Android.Bluetooth.Gatt, characteristic: Android.Bluetooth.GattCharacteristic, status: Android.Bluetooth.Gatt.Status)
+    
+    func onConnectionStateChange(gatt: Android.Bluetooth.Gatt, status: Android.Bluetooth.Gatt.Status, newStatus: Android.Bluetooth.Gatt.Status)
+}
