981562bb5c1b0d730a800deb09ccd2e8baf454b4
jmarkstar <mestrella.1991@gmail.com>
2018-06-18 12:28:32 -0500

    Upgraded `PureSwift/Bluetooth` Library.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/SwiftJava/java_swift.git", versions: Version(2,1,1) ..< Version(3,0,0)),
         .Package(url: "https://github.com/SwiftJava/java_util.git", majorVersion: 2),
-        .Package(url: "https://github.com/PureSwift/Bluetooth.git", majorVersion: 1),
+        .Package(url: "https://github.com/PureSwift/Bluetooth.git", majorVersion: 2),
         .Package(url: "https://github.com/PureSwift/JNI.git", majorVersion: 1)
     ]
 )
