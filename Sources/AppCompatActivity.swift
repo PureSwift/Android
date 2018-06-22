45d113dc6cb30c373999e1e2d4224726be2eeb81
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-06-22 18:10:43 -0500

    Working on Activity

--- Sources/AppCompatActivity.swift
+++ Sources/AppCompatActivity.swift
@@ -15,12 +15,12 @@ public extension SwiftSupport.App {
 }
 
 /**
- <# Type / Member Name#>
+ Activity
  
  <#Description#>
  */
-protocol SwiftSupportAppCompatActivity {
-        
+public protocol SwiftSupportAppCompatActivity {
+    
     func onCreate(savedInstanceState: Android.OS.Bundle?)
     
     func onStart()
@@ -40,7 +40,7 @@ protocol SwiftSupportAppCompatActivity {
     func onRequestPermissionsResult(requestCode: Int, permissions: [String], grantResults: [Int])
 }
 
-extension SwiftSupportAppCompatActivity {
+public extension SwiftSupportAppCompatActivity {
     
     func onCreate(savedInstanceState: Android.OS.Bundle?) {
         
