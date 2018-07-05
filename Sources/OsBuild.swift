c81c7aebe02005ccfa2161b7e349b693b1374351
jmarkstar <mestrella.1991@gmail.com>
2018-07-05 12:15:48 -0500

    Finished AndroidBuild

--- Sources/OsBuild.swift
+++ Sources/OsBuild.swift
@@ -52,6 +52,299 @@ public class AndroidBuild: JavaObject {
     }
 }
 
+// MARK: Constants
+
+internal extension AndroidBuild {
+    
+    internal static var UNKNOWN: String {
+        
+        get {
+            
+            let __value = JNIField.GetStaticObjectField(
+                fieldName: "UNKNOWN",
+                fieldType: "Ljava/lang/String;",
+                fieldCache: &JNICache.FieldID.UNKNOWN,
+                className: JNICache.className,
+                classCache: &JNICache.jniClass )
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+}
+
+// MARK: Fields
+
+public extension AndroidBuild {
+    
+    public var BOARD: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "BOARD",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.BOARD,
+                                                  object: javaObject)
+    
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var BOOTLOADER: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "BOOTLOADER",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.BOOTLOADER,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var BRAND: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "BRAND",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.BRAND,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var DEVICE: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "DEVICE",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.DEVICE,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var DISPLAY: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "DISPLAY",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.DISPLAY,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var FINGERPRINT: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "FINGERPRINT",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.FINGERPRINT,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var HARDWARE: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "HARDWARE",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.HARDWARE,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var HOST: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "HOST",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.HOST,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var ID: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "ID",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.ID,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var MANUFACTURER: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "MANUFACTURER",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.MANUFACTURER,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var MODEL: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "MODEL",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.MODEL,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var PRODUCT: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "PRODUCT",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.PRODUCT,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var SUPPORTED_32_BIT_ABIS: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "SUPPORTED_32_BIT_ABIS",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.SUPPORTED_32_BIT_ABIS,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var SUPPORTED_64_BIT_ABIS: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "SUPPORTED_64_BIT_ABIS",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.SUPPORTED_64_BIT_ABIS,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var SUPPORTED_ABIS: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "SUPPORTED_ABIS",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.SUPPORTED_ABIS,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var TAGS: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "TAGS",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.TAGS,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var TIME: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "TIME",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.TIME,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var TYPE: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "TYPE",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.PRODUCT,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+    
+    public var USER: String {
+        
+        get {
+            let __value = JNIField.GetObjectField(fieldName: "USER",
+                                                  fieldType: "Ljava/lang/String;",
+                                                  fieldCache: &JNICache.FieldID.USER,
+                                                  object: javaObject)
+            
+            defer { JNI.DeleteLocalRef(__value) }
+            
+            return String(javaObject: __value)
+        }
+    }
+}
+
 // MARK: - JNI
 
 internal extension AndroidBuild {
@@ -77,6 +370,7 @@ internal extension AndroidBuild {
             static var DEVICE: jfieldID?
             static var DISPLAY: jfieldID?
             static var FINGERPRINT: jfieldID?
+            static var HARDWARE: jfieldID?
             static var HOST: jfieldID?
             static var ID: jfieldID?
             static var MANUFACTURER: jfieldID?
