52f5de452537f685391d9062d36512f1faa20cb5
jmarkstar <mestrella.1991@gmail.com>
2018-11-08 16:50:39 -0500

    Removed Bluetooth module.

--- Package.swift
+++ Package.swift
@@ -5,21 +5,15 @@ let package = Package(
     name: "Android",
     products: [
         .library(name: "Android", targets: ["Android"]),
-        .library(name: "AndroidBluetooth", targets: ["AndroidBluetooth"])
     ],
     dependencies: [
         .package(url: "https://github.com/SwiftJava/java_util.git", .branch("master")),
         .package(url: "https://github.com/PureSwift/JNI.git", .branch("master")),
-        .package(url: "https://github.com/PureSwift/Bluetooth.git", .branch("master"))
     ],
     targets: [
         .target(
             name: "Android",
             dependencies: ["java_util", "JNI"]
-        ),
-        .target(
-            name: "AndroidBluetooth",
-            dependencies: ["Android", "Bluetooth"]
         )
     ]
 )
