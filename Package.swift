5db424f75da95e106c49fc3f229ed60f2e77eb20
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-07 00:01:29 -0400

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -1,25 +1,50 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
-    name: "Android",
+    name: "SwiftAndroid",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
-        .library(name: "Android", targets: ["Android"]),
+        .library(
+            name: "SwiftAndroid", targets: ["SwiftAndroid"]
+        )
     ],
     dependencies: [
         .package(
-            url: "https://github.com/PureSwift/java_util.git",
+            url: "https://github.com/swiftlang/swift-java.git",
+            branch: "main"
+        ),
+        // Deprecated
+        .package(
+            url: "https://github.com/PureSwift/JNI.git",
             branch: "master"
         ),
         .package(
-            url: "https://github.com/PureSwift/JNI.git",
+            url: "https://github.com/PureSwift/java_util.git",
             branch: "master"
         )
     ],
     targets: [
         .target(
-            name: "Android",
-            dependencies: ["java_util", "JNI"]
+            name: "SwiftAndroid",
+            dependencies: [
+                .product(
+                    name: "JavaKit",
+                    package: "swift-java"
+                ),
+                .product(
+                    name: "JNI",
+                    package: "JNI"
+                ),
+                .product(
+                    name: "java_util",
+                    package: "java_util"
+                )
+            ],
+            path: "Sources/Android"
         )
-    ]
+    ],
+    swiftLanguageModes: [.v5]
 )
