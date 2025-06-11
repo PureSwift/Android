2d62fbf55a09f5b83e786c6321d07a18909e06e8
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-11 01:52:38 -0400

    Add `Runnable`

---
+++ Demo/app/src/main/swift/Runnable.swift
@@ -0,0 +1,50 @@
+//
+//  Runnable.swift
+//  AndroidSwiftUI
+//
+//  Created by Alsey Coleman Miller on 6/9/25.
+//
+
+import JavaKit
+import JavaRuntime
+import AndroidKit
+import AndroidJavaLang
+
+@JavaClass("com.pureswift.swiftandroid.Runnable", extends: AndroidJavaLang.Runnable.self)
+open class Runnable: JavaObject {
+    
+    public typealias Block = () -> ()
+    
+    @JavaMethod
+    @_nonoverride public convenience init(block: SwiftObject?, environment: JNIEnvironment? = nil)
+    
+    @JavaMethod
+    public func getBlock() -> SwiftObject?
+}
+
+public extension Runnable {
+    
+    convenience init(_ block: @escaping () -> Void, environment: JNIEnvironment? = nil) {
+        let object = SwiftObject(block, environment: environment)
+        self.init(block: object, environment: environment)
+    }
+}
+
+@JavaImplementation("com.pureswift.swiftandroid.Runnable")
+extension Runnable {
+    
+    @JavaMethod
+    func run() {
+        block()
+    }
+}
+
+private extension Runnable {
+    
+    var block: Block {
+        guard let block = getBlock()?.valueObject().value as? Block else {
+            fatalError()
+        }
+        return block
+    }
+}
