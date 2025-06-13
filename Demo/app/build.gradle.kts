dc1a17ed9efe46bf9fac26772283b55a481d2404
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-13 00:15:17 -0400

    Update dependencies

--- Demo/app/build.gradle.kts
+++ Demo/app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
