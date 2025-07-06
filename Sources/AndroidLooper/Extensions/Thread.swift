59ea796053b20f7f4b3a7aaf476f19309293ef5e
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-06 13:51:33 -0400

    Add `Thread` extensions

---
+++ Sources/AndroidLooper/Extensions/Thread.swift
@@ -0,0 +1,26 @@
+//
+//  Thread.swift
+//  SwiftAndroid
+//
+//  Created by Alsey Coleman Miller on 7/6/25.
+//
+
+import Foundation
+
+public extension Thread {
+    
+    /**
+     * Prepares a looper associated with the calling thread, and returns it.
+     * If the thread already has a looper, it is returned.  Otherwise, a new
+     * one is created, associated with the thread, and returned.
+     *
+     * The opts may be `ALOOPER_PREPARE_ALLOW_NON_CALLBACKS` or 0.
+     */
+    static func withLooper<T, E>(
+        options: Looper.PrepareOptions = [],
+        _ body: (borrowing Looper) throws(E) -> T
+    ) throws(E) -> T {
+        let looper = Looper.currentThread(options: options)
+        return try body(looper)
+    }
+}
