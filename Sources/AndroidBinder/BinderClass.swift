b961b4c57327af056f39cf476ab0d5e19951f2d5
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-06 04:31:30 -0400

    Add `BinderClass`

---
+++ Sources/AndroidBinder/BinderClass.swift
@@ -0,0 +1,16 @@
+//
+//  BinderClass.swift
+//  SwiftAndroid
+//
+//  Created by Alsey Coleman Miller on 7/6/25.
+//
+
+#if canImport(Android)
+import Android
+import AndroidNDK
+#endif
+
+public struct BinderClass: ~Copyable {
+    
+    
+}
