a4d9cdd401a10c5e4221add9e172c44ad5b95879
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-06 02:47:12 -0400

    Update `AndroidBinderError.ErrorCode`

--- Sources/AndroidBinder/Error.swift
+++ Sources/AndroidBinder/Error.swift
@@ -82,13 +82,24 @@ public extension AndroidBinderError {
 
 public extension AndroidBinderError.ErrorCode {
     
-    static var unknown: AndroidBinderError.ErrorCode { .init(STATUS_UNKNOWN_ERROR) } // INT32_MIN value
-    
-    static var noMemory: AndroidBinderError.ErrorCode { .init(STATUS_NO_MEMORY) }
-    
-    static var invalidOperation: AndroidBinderError.ErrorCode { .init(STATUS_INVALID_OPERATION) }
-    
-    static var badValue: AndroidBinderError.ErrorCode { .init(STATUS_BAD_VALUE) }
+    static var unknown: Self { .init(STATUS_UNKNOWN_ERROR) } // INT32_MIN
+    static var noMemory: Self { .init(STATUS_NO_MEMORY) } // -ENOMEM
+    static var invalidOperation: Self { .init(STATUS_INVALID_OPERATION) } // -ENOSYS
+    static var badValue: Self { .init(STATUS_BAD_VALUE) } // -EINVAL
+    static var badType: Self { .init(STATUS_BAD_TYPE) } // (STATUS_UNKNOWN_ERROR + 1)
+    static var nameNotFound: Self { .init(STATUS_NAME_NOT_FOUND) } // -ENOENT
+    static var permissionDenied: Self { .init(STATUS_PERMISSION_DENIED) } // -EPERM
+    static var noInit: Self { .init(STATUS_NO_INIT) } // -ENODEV
+    static var alreadyExists: Self { .init(STATUS_ALREADY_EXISTS) } // -EEXIST
+    static var deadObject: Self { .init(STATUS_DEAD_OBJECT) } // -EPIPE
+    static var failedTransaction: Self { .init(STATUS_FAILED_TRANSACTION) } // (STATUS_UNKNOWN_ERROR + 2)
+    static var badIndex: Self { .init(STATUS_BAD_INDEX) } // -EOVERFLOW
+    static var notEnoughData: Self { .init(STATUS_NOT_ENOUGH_DATA) } // -ENODATA
+    static var wouldBlock: Self { .init(STATUS_WOULD_BLOCK) } // -EWOULDBLOCK
+    static var timedOut: Self { .init(STATUS_TIMED_OUT) } // -ETIMEDOUT
+    static var unknownTransaction: Self { .init(STATUS_UNKNOWN_TRANSACTION) } // -EBADMSG
+    static var fdsNotAllowed: Self { .init(STATUS_FDS_NOT_ALLOWED) } // (STATUS_UNKNOWN_ERROR + 7)
+    static var unexpectedNull: Self { .init(STATUS_UNEXPECTED_NULL) } // (STATUS_UNKNOWN_ERROR + 8)
 }
 
 // MARK: - Internal Extensions
