920f7167873d0e56f719854a108ee917bbd3221e
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-02-13 23:16:21 -0500

    Updated Android NDK headers

--- Sources/AndroidNDK/include/ndk.h
+++ Sources/AndroidNDK/include/ndk.h
@@ -6,7 +6,6 @@
 #include <android/asset_manager.h>
 #include <android/binder_ibinder_jni.h>
 #include <android/binder_ibinder.h>
-#include <android/binder_internal_logging.h>
 #include <android/binder_parcel_jni.h>
 #include <android/binder_parcel.h>
 #include <android/binder_status.h>
