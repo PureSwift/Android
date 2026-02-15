45bdde17d1e249ec60579dfa72bed63a9449db61
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-02-15 15:17:17 -0500

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -79,8 +79,8 @@ var package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/PureSwift/swift-java.git",
-            branch: "feature/android"
+            url: "https://github.com/swiftlang/swift-java.git",
+            branch: "main"
         ),
         .package(
             url: "https://github.com/PureSwift/JavaLang.git",
