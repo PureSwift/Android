8765e42152cf37443c04178f5ffe2db679ac1cad
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-02-26 20:14:42 -0500

    Add `AndroidHardware` target

--- Package.swift
+++ Package.swift
@@ -75,6 +75,9 @@ var package = Package(
         ),
         .library(
             name: "AndroidNDK", targets: ["AndroidNDK"]
+        ),
+        .library(
+            name: "AndroidHardware", targets: ["AndroidHardware"]
         )
     ],
     dependencies: [
@@ -444,6 +447,21 @@ var package = Package(
             cxxSettings: [
                 .define("ANDROID_NDK_VERSION", to: ndkVersion.description),
                 .define("ANDROID_SDK_VERSION", to: sdkVersion.description)
+            ],
+            linkerSettings: [
+                .linkedLibrary("android", .when(platforms: [.android]))
+            ]
+        ),
+        .target(
+            name: "AndroidHardware",
+            dependencies: [
+                "AndroidNDK",
+                "AndroidLooper"
+            ],
+            swiftSettings: [
+              .swiftLanguageMode(.v6),
+              ndkVersionDefine,
+              sdkVersionDefine
             ],
             linkerSettings: [
                 .linkedLibrary("android", .when(platforms: [.android]))
